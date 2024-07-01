529b7e0c5224bf0753705da889845554b49e658d
andrey.tis <andrey.tis.rb@gmail.com>
2024-07-01 15:59:06 +0300

    Commit demo story_book app

---
+++ packages/component_library/component_show_app/android/app/src/main/kotlin/com/example/component_show_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.component_show_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ packages/component_library/component_show_app/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:component_show_app/story_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(StoryApp());
+}
+
