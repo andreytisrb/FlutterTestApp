7083a8845c6905a81d018d2d332f8f504ac4d586
andrey.tis <andrey.tis.rb@gmail.com>
2024-07-03 13:15:58 +0300

    Commit forms and hive storage

---
+++ packages/form_fields/test/form_fields_test.dart
@@ -0,0 +1,9 @@
+
+import 'package:form_fields/form_fields.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('adds one to input values', () {
+    expect(1, 1);
+  });
+}
---
+++ packages/key_value_storage/test/key_value_storage_test.dart
@@ -0,0 +1,9 @@
+
+import 'package:key_value_storage/key_value_storage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+  });
+}
